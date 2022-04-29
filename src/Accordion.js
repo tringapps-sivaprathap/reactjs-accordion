b18c64c757fc738b4aea694a5415b066d1f0601a
tringapps-sivaprathap <sivaprathap.s@tringapps.com>
2022-04-29 22:39:02 +0530

    changed to open one tab at a time

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,27 +1,51 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Accordion = ({ user }) => {
-    const [show, setShow] = useState(false)
+const Accordion = ({ users }) => {
+	const [clicked, setClicked] = useState(null)
+    const toggle = (user) => {
+        // if clicked second time {display nothing} else {display correspnding}
+        if(clicked === user.id) {
+            setClicked(null)
+            localStorage.removeItem("currentuser")
+        }
+        else {
+            setClicked(user.id)
+            localStorage.setItem("currentuser", JSON.stringify(user))
+        }
+    }
 
     return (
-    <div className="accordion">
-        <div onClick={() => {
-            setShow(!show)
-            localStorage.setItem("currentuser", JSON.stringify(user))
-        }} className="accordion-button">
-            <span>{user.first_name}</span>
-            {show ? <span>&#11165;</span> : <span>&#11167;</span>}
-        </div>
+        <> 
+            {users.map((user) => {
+                return (
+                    <div key={user.id} className="accordion">
+                        <div onClick = {() => {toggle(user)}}
+                        className="accordion-button">
+                            <span>{user.first_name}</span>
+                            {clicked === user.id ? <span>&#11165;</span> : <span>&#11167;</span>}
+                        </div>
 
-        {show && <div className="accordion-content">
-            <img src={user.avatar} alt={user.first_name} />
-            <div className="details">
-                <div><span className="details-labels">First Name:</span> <span>{user.first_name}</span></div>
-                <div><span className="details-labels">Last Name:</span> <span>{user.last_name}</span></div>
-                <div><span className="details-labels">Email:</span> <span>{user.email}</span></div>
-            </div>
-        </div>}
-    </div>
+                        {clicked === user.id && (<div className="accordion-content">
+                            <img src={user.avatar} alt={user.first_name} />
+                            <div className="details">
+                                <div>
+                                    <span className="details-labels">First Name:</span>{" "}
+                                    <span>{user.first_name}</span>
+                                </div>
+                                <div>
+                                    <span className="details-labels">Last Name:</span>{" "}
+                                    <span>{user.last_name}</span>
+                                </div>
+                                <div>
+                                    <span className="details-labels">Email:</span>{" "}
+                                    <span>{user.email}</span>
+                                </div>
+                            </div>
+                        </div>)}
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import Accordion from './Accordion'
+import { useState, useEffect } from "react";
+import Accordion from "./Accordion";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  // initiate usestate with an empty string (cause map() needs an array)
-  
+  const [users, setUsers] = useState([])
+
   useEffect(() => {
     fetch("https://reqres.in/api/users")
-    .then((response) => response.json())
-    .then((items) => setUsers(items.data))
+      .then((response) => response.json())
+      .then((items) => setUsers(items.data))
   }, [])
 
   return (
     <>
-      <div className='header'><p>Employees Accordion</p></div>
-      {users.map((user) => {return <Accordion key={user.id} user={user} />})}
+      <div className="header">
+        <p>Employees Accordion</p>
+      </div>
+      <Accordion users={users}/>
     </>
   )
 }
