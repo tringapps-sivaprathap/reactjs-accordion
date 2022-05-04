43a09adb09a890430aeaf00187f7c1a8b75846b7
Sivaprathap S <102897756+tringapps-sivaprathap@users.noreply.github.com>
2022-05-04 13:15:13 +0530

    updated error message

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import Accordion from "./components/Accordion";
 
 const App = () => {
   const [users, setUsers] = useState([])
+  const [render, setRender] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     const site = "https://reqres.in";
     fetch(`${site}/api/users`)
       .then((response) => response.json())
       .then((items) => setUsers(items.data))
-  }, [])
+      .catch((error) => {
+        setRender(false);
+        setErrorMessage(error.message);
+      })
+  }, [setRender, setErrorMessage])
 
   return (
     <>
       <div className="header">
         <p>Employees Accordion</p>
       </div>
-      <Accordion users={users}/>
+      {render && <Accordion users={users}/>}
+      {!render && <p className="fetch-error">{errorMessage}</p>}
     </>
   )
 }
